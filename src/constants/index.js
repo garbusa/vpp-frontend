7437210e0e65cbb708e6ff26681412189d68c1bd
Nick Garbusa <nick.garbusa@uni-oldenburg.de>
2021-05-31 10:09:05 +0200

    localhost

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
  * Die URLs zu den einzelnen Services
  * @type {string}
  */
-export const MASTERDATA_SERVICE_URL = 'http://68.183.223.32:8081';
-export const LOAD_SERVICE_URL = 'http://68.183.223.32:8082';
-export const PRODUCTION_SERVICE_URL = 'http://68.183.223.32:8083';
-export const ACTION_SERVICE_URL = 'http://68.183.223.32:8084';
+export const MASTERDATA_SERVICE_URL = 'http://localhost:8081';
+export const LOAD_SERVICE_URL = 'http://localhost:8082';
+export const PRODUCTION_SERVICE_URL = 'http://localhost:8083';
+export const ACTION_SERVICE_URL = 'http://localhost:8084';
